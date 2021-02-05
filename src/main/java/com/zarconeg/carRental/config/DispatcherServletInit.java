8785f2b5204b610e2fea014e879d43dd4418d200
Gabriele Zarcone <gabriele.zarcone0@gmail.com>
2021-02-05 10:00:15 +0100

    File di configurazione

---
+++ src/main/java/com/zarconeg/carRental/config/DispatcherServletInit.java
@@ -0,0 +1,24 @@
+package com.zarconeg.carRental.config;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+public class DispatcherServletInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{BeanConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
