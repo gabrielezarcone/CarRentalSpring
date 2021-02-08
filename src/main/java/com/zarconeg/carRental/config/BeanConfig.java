6dc6d9ac2652f118e6b4ae2987f8c96aa512a1b2
Gabriele Zarcone <gabriele.zarcone0@gmail.com>
2021-02-08 12:31:56 +0100

    Configurazione Tiles

--- src/main/java/com/zarconeg/carRental/config/BeanConfig.java
+++ src/main/java/com/zarconeg/carRental/config/BeanConfig.java
@@ -6,24 +6,39 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.view.InternalResourceView;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesViewResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.zarconeg.carRental")
-public class BeanConfig {
+public class BeanConfig implements WebMvcConfigurer {  // implemento l'interfaccia per cambiare le proprietà di default. Altrimenti potevo non farlo.
+
+    // Tiles come view resolver ---------------------------------------
     @Bean
-    public ViewResolver viewResolver(){
-        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-        viewResolver.setViewClass(JstlView.class);
-        viewResolver.setPrefix("/WEB-INF/views");
-        viewResolver.setSuffix(".jsp");
-        return viewResolver;
+    public TilesConfigurer tilesConfigurer(){  //configuro le proprietà di tiles
+        TilesConfigurer tilesConfigurer = new TilesConfigurer();
+        tilesConfigurer.setDefinitions(new String[] {"WEB-INF/views/**/tiles.xml"});
+        tilesConfigurer.setCheckRefresh(true);
+        return tilesConfigurer;
     }
 
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry){  //Sostituisco il viewresolver di default con Tiles che diventa il nuovo viewresolver
+        TilesViewResolver viewResolver = new TilesViewResolver();
+        registry.viewResolver(viewResolver);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {  //Indico la posizione dei file statici (CSS, JS...)
+        registry.addResourceHandler("/static/**").addResourceLocations("/static");
+    }
+
+    // Messaggi --------------------------------------------------------
     @Bean
     public MessageSource messageSource(){
         ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
