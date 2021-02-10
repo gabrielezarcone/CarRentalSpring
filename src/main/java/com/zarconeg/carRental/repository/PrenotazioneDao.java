3ec30570729353272f839118871c30f67d058190
Gabriele Zarcone <gabriele.zarcone0@gmail.com>
2021-02-10 10:01:39 +0100

    Annotazione @Repository sulle classi DAO

--- src/main/java/com/zarconeg/carRental/repository/PrenotazioneDao.java
+++ src/main/java/com/zarconeg/carRental/repository/PrenotazioneDao.java
@@ -1,6 +1,8 @@
 package com.zarconeg.carRental.repository;
 
 import com.zarconeg.carRental.domain.Prenotazione;
+import org.springframework.stereotype.Repository;
 
+@Repository("prenotazioneDao")
 public class PrenotazioneDao extends AbstractDao<Long, Prenotazione>{
 }
