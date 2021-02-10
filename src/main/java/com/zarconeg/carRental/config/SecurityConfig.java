5b3b5f1525a9afa18fff6e47861b01b53fe328e2
Gabriele Zarcone <gabriele.zarcone0@gmail.com>
2021-02-10 17:38:50 +0100

    Configurazione Spring Security (TODO ruoli)

---
+++ src/main/java/com/zarconeg/carRental/config/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.zarconeg.carRental.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+
+/** Questa classe crea un Servlet Filter chiamato springSecurityFilterChain  all WAR */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    @Qualifier("customUserDetailsService")
+    UserDetailsService userDetailsService;
+
+    @Autowired
+    public void configureGlobalSecuritu(AuthenticationManagerBuilder auth) throws Exception{
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder()); // mi consente di criptare le password
+    }
+
+    @Override
+    protected void configure(HttpSecurity http)throws Exception{
+        http.authorizeRequests()
+                .antMatchers( "/home").permitAll()
+                .antMatchers("/","/admin/**").access("hasRole('ADMIN')")
+                .antMatchers("/customer/**").access("hasRole('ADMIN') and hasRole('CUSTOMER')")
+                .and().formLogin()
+                .and().exceptionHandling().accessDeniedPage("/Access_Denied");
+    }
+
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        // Bean che mi consente di criptare le password
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ src/main/java/com/zarconeg/carRental/service/CustomUserDetailsService.java
@@ -0,0 +1,43 @@
+package com.zarconeg.carRental.service;
+
+import com.zarconeg.carRental.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Service("customUserDetailsService")
+public class CustomUserDetailsService implements UserDetailsService { // l'interfaccia mi consente di recuperare le info degli utenti
+    @Autowired
+    private UserService userService;
+
+
+    @Override
+    @Transactional(readOnly=true)
+    public UserDetails loadUserByUsername(String ssoId) throws UsernameNotFoundException {
+        // https://www.boraji.com/spring-mvc-5-spring-security-5-hibernate-5-example
+        User user = userService.getByUsername(ssoId);
+        if (user==null){
+            throw new UsernameNotFoundException("Utente non trovato");
+        }
+        else{
+            // Creo un utente di Core security
+            org.springframework.security.core.userdetails.User.UserBuilder builder;
+            builder =  org.springframework.security.core.userdetails.User.withUsername(ssoId);
+            builder.password(user.getPassword());
+            builder.disabled(user.isDeleted());
+/*          TODO implementare una relazione Autorizzazioni con User per poter settare le autorizzazioni come segue
+            String[] authorities = user.getAuthorities()
+                    .stream().map(a -> a.getAuthority()).toArray(String[]::new);
+            builder.authorities(authorities);*/
+            return builder.build();
+        }
+    }
+}
