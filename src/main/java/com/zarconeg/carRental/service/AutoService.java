ce535b09b5ba5590ba14fbe7704f6ebee31db525
Gabriele Zarcone <gabriele.zarcone0@gmail.com>
2021-02-12 16:56:12 +0100

    Import corretto di @Transactional spring e non javax

--- src/main/java/com/zarconeg/carRental/service/AutoService.java
+++ src/main/java/com/zarconeg/carRental/service/AutoService.java
@@ -4,8 +4,8 @@ import com.zarconeg.carRental.domain.Auto;
 import com.zarconeg.carRental.repository.AutoDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 import java.util.List;
 
 @Service("autoService")
