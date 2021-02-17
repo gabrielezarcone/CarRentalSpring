614b0da86bb0b8b0af0df275e43bab518ec0a2f9
Gabriele Zarcone <gabriele.zarcone0@gmail.com>
2021-02-17 10:11:46 +0100

    Dao e Service per aggiornare stato prenotazione

--- src/main/java/com/zarconeg/carRental/repository/PrenotazioneDao.java
+++ src/main/java/com/zarconeg/carRental/repository/PrenotazioneDao.java
@@ -5,4 +5,13 @@ import org.springframework.stereotype.Repository;
 
 @Repository("prenotazioneDao")
 public class PrenotazioneDao extends AbstractDao<Long, Prenotazione>{
+
+    public void updateStato(Prenotazione prenotazione, Prenotazione.Stato stato){
+        prenotazione.setStato(stato);
+    }
+
+    public void updateStatoAsString(Prenotazione prenotazione, String statoString){
+        Prenotazione.Stato enumStato = Prenotazione.Stato.valueOf(statoString);
+        updateStato(prenotazione, enumStato);
+    }
 }
