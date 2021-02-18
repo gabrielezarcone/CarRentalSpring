4eae03a8d6644fd27e64312d0b00383ffad70dce
Gabriele Zarcone <gabriele.zarcone0@gmail.com>
2021-02-18 10:19:26 +0100

    Customer può cancellare le proprie prenotazioni

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -54,3 +54,12 @@ function cercaUtente(urlListaUtenti){
     let url = urlListaUtenti+"/cerca/"+filtro+"/"+testo
     location.replace(url)
 }
+
+function eliminaPrenotazione(idPrenotazione){
+    var method = 'GET';
+    var url = './ajax/eliminaPrenotazione/'+idPrenotazione;
+    var onloadfunction = function () {
+        location.reload();
+    }
+    xhrCall(method,url,null,onloadfunction);
+}
