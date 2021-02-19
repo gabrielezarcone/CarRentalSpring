fefbd6022064531da57dfebd2a49d1c5592fa924
Gabriele Zarcone <gabriele.zarcone0@gmail.com>
2021-02-19 09:48:48 +0100

    Admin può modificare auto

--- src/main/java/com/zarconeg/carRental/controllers/ModificaAutoController.java
+++ src/main/java/com/zarconeg/carRental/controllers/ModificaAutoController.java
@@ -1,13 +1,18 @@
 package com.zarconeg.carRental.controllers;
 
 import com.zarconeg.carRental.domain.Auto;
+import com.zarconeg.carRental.domain.User;
 import com.zarconeg.carRental.service.AutoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/admin/")
@@ -22,4 +27,15 @@ public class ModificaAutoController {
         model.addAttribute("auto", auto);
         return "modificaAuto";
     }
+
+    // Gestione forms ------------------------------------------------------------------------------------
+    @PostMapping("modificaAuto/{idAuto}")
+    public String formModificaAuto(@Valid Auto auto, BindingResult result, ModelMap model){
+        if(result.hasErrors()) {
+            return modificaAuto(auto.getId(),model);
+        }
+        autoService.aggiungiAggiorna(auto);
+        return "redirect:/parcoAuto/";
+    }
+
 }
