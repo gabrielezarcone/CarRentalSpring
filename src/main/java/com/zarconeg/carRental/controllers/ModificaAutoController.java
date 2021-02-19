e83789b635837d4d1815c06605b0aaa6bc44621a
Gabriele Zarcone <gabriele.zarcone0@gmail.com>
2021-02-19 09:33:30 +0100

    Form di modifica auto

--- src/main/java/com/zarconeg/carRental/controllers/ModificaAutoController.java
+++ src/main/java/com/zarconeg/carRental/controllers/ModificaAutoController.java
@@ -1,5 +1,8 @@
 package com.zarconeg.carRental.controllers;
 
+import com.zarconeg.carRental.domain.Auto;
+import com.zarconeg.carRental.service.AutoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,8 +13,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/admin/")
 public class ModificaAutoController {
 
+    @Autowired
+    AutoService autoService;
+
     @GetMapping("modificaAuto/{idAuto}")
     public String modificaAuto(@PathVariable long idAuto, ModelMap model){
+        Auto auto = autoService.getById(idAuto);
+        model.addAttribute("auto", auto);
         return "modificaAuto";
     }
 }
